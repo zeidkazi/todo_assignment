7029f8d41b96896a9fe2acfce16688340d20c47d
ZEID KAZI <zeidkazi7860@gmail.com>
2025-02-14 18:29:18 +0530

    chore: fix pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,18 +1,30 @@
-import { ChevronLeft, ChevronRight } from 'lucide-react'
-import { PaginationType } from '../types'
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { PaginationType } from "../types";
+import { useContext } from "react";
+import { MyContext } from "../context/Context";
 
-const Pagination = ({data,page,setPage}:PaginationType) => {
+const Pagination = ({ data }: PaginationType) => {
+  const { page, setPage } = useContext(MyContext);
+
   return (
     <div className="w-full flex items-center justify-center gap-xlarge ">
-    <button disabled={!data?.prev} className="cursor-pointer disabled:text-gray-500 disabled:cursor-not-allowed" onClick={()=>setPage((prev)=>prev-1)}>
-      <ChevronLeft />
-    </button>
-    <p>{page}</p>
-    <button disabled={!data?.next} className="cursor-pointer disabled:text-gray-500 disabled:cursor-not-allowed" onClick={()=>setPage((prev)=>prev+1)}>
-      <ChevronRight />
-    </button>
-  </div>
-  )
-}
+      <button
+        disabled={!data?.prev}
+        className="cursor-pointer disabled:text-gray-500 disabled:cursor-not-allowed"
+        onClick={() => setPage((prev) => prev - 1)}
+      >
+        <ChevronLeft />
+      </button>
+      <p>{page}</p>
+      <button
+        disabled={!data?.next}
+        className="cursor-pointer disabled:text-gray-500 disabled:cursor-not-allowed"
+        onClick={() => setPage((prev) => prev + 1)}
+      >
+        <ChevronRight />
+      </button>
+    </div>
+  );
+};
 
-export default Pagination
+export default Pagination;

d5057d6c539874686869b65188b68fbd51019b50
ZEID KAZI <zeidkazi7860@gmail.com>
2025-02-14 18:44:44 +0530

    refactor: optimistic edit update

--- src/components/EditModal.tsx
+++ src/components/EditModal.tsx
@@ -1,5 +1,5 @@
 import { Controller, useForm } from "react-hook-form";
-import { EditModalProps } from "../types";
+import { DataType, EditModalProps } from "../types";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { formSchema } from "./AddTodo";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
@@ -7,8 +7,12 @@ import { editTodo } from "../api/api";
 import DatePicker from "react-datepicker";
 import toast from "react-hot-toast";
 import { X } from "lucide-react";
+import { useContext } from "react";
+import { MyContext } from "../context/Context";
 
 const EditModal = ({ setClose, todo }: EditModalProps) => {
+  const { page: currentPage, setPage } = useContext(MyContext);
+
   const {
     register,
     handleSubmit,
@@ -23,6 +27,38 @@ const EditModal = ({ setClose, todo }: EditModalProps) => {
 
   const { mutate } = useMutation({
     mutationFn: editTodo,
+    onMutate: async (updatedTodo) => {
+      
+      await queryClient.cancelQueries({ queryKey: ["todo"] });
+
+      const prevTodos = queryClient.getQueryData<DataType>([
+        "todo",
+        { page: currentPage },
+      ]);
+      console.log("clicked todo:", updatedTodo, "actual data", prevTodos);
+
+      queryClient.setQueryData(
+        ["todo", { page: currentPage }],
+        (old?: DataType) => {
+          return {
+            ...old,
+            data: old?.data.map((todo) =>
+              todo.id === updatedTodo.id ? { ...todo, ...updatedTodo.todo } : todo
+            ),
+          };
+        }
+      );
+      return { prevTodos };
+    },
+
+    onError: (errors, variables, context) => {
+      if (context?.prevTodos) {
+        queryClient.setQueryData(
+          ["todo", { page: currentPage }],
+          context.prevTodos
+        );
+      }
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["todo"] });
       setClose(false);
@@ -37,7 +73,7 @@ const EditModal = ({ setClose, todo }: EditModalProps) => {
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="relative min-w-[500px] min-h-[100px] bg-white rounded-lg p-base shadow-lg flex flex-col items-center gap-base"
+        className="relative min-w-[500px] min-h-[120px] bg-white rounded-lg p-base shadow-lg flex flex-col items-center gap-base"
       >
         <button
           onClick={() => setClose(false)}
@@ -49,22 +85,27 @@ const EditModal = ({ setClose, todo }: EditModalProps) => {
         <p className="text-xlarge font-bold">Edit ToDo</p>
         <form
           onSubmit={handleSubmit((data) => mutate({ id: todo.id, todo: data }))}
-          className="w-full max-w-xl flex flex-col items-start gap-xs"
+          className="w-full max-w-xl flex flex-col items-start gap-base"
         >
           <div className=" w-full flex items-center gap-base">
             <label htmlFor="title" className="w-32 text-base font-semibold">
               Title
             </label>
-            <input
-              type="text"
-              id="title"
-              {...register("title")}
-              className={`grow  border rounded p-xs ${
-                errors?.title
-                  ? "focus:border-red-500"
-                  : "focus:outline-purple-500"
-              }`}
-            />
+            <div className="grow">
+              <input
+                type="text"
+                id="title"
+                {...register("title")}
+                className={`w-full  border rounded p-xs ${
+                  errors?.title
+                    ? "focus:border-red-500"
+                    : "focus:outline-purple-500"
+                }`}
+              />
+              {errors && errors.title ? (
+                <p className="text-xs text-red-500">{errors.title?.message}</p>
+              ) : null}
+            </div>
           </div>
           <div className=" w-full flex items-center gap-base">
             <label
@@ -73,36 +114,50 @@ const EditModal = ({ setClose, todo }: EditModalProps) => {
             >
               Description
             </label>
-            <input
-              type="text"
-              id="description"
-              {...register("description")}
-              className={`grow  border rounded p-xs ${
-                errors?.description
-                  ? "focus:border-red-500"
-                  : "focus:outline-purple-500"
-              }`}
-            />
+            <div className="grow">
+              <input
+                type="text"
+                id="description"
+                {...register("description")}
+                className={`w-full  border rounded p-xs ${
+                  errors?.description
+                    ? "focus:border-red-500"
+                    : "focus:outline-purple-500"
+                }`}
+              />
+              {errors && errors.description ? (
+                <p className="text-xs text-red-500">
+                  {errors.description?.message}
+                </p>
+              ) : null}
+            </div>
           </div>
           <div className=" w-full flex items-center gap-base">
             <label htmlFor="title" className="w-32 text-base font-semibold">
               Time
             </label>
-            <Controller
-              name="time"
-              control={control}
-              render={({ field }) => (
-                <DatePicker
-                  selected={field.value}
-                  onChange={(date) => field.onChange(date)}
-                  className={`w-full p-xs border rounded-md  ${
-                    errors.time ? "border-red-500" : "focus:outline-purple-500"
-                  }`}
-                  placeholderText="Enter Task Date"
-                  dateFormat="dd MMM yyyy"
-                />
-              )}
-            />
+            <div className="grow">
+              <Controller
+                name="time"
+                control={control}
+                render={({ field }) => (
+                  <DatePicker
+                    selected={field.value}
+                    onChange={(date) => field.onChange(date)}
+                    className={`w-full p-xs border rounded-md  ${
+                      errors.time
+                        ? "border-red-500"
+                        : "focus:outline-purple-500"
+                    }`}
+                    placeholderText="Enter Task Date"
+                    dateFormat="dd MMM yyyy"
+                  />
+                )}
+              />
+              {errors && errors.time ? (
+                <p className="text-xs text-red-500">{errors.time?.message}</p>
+              ) : null}
+            </div>
           </div>
           <div className="w-full flex items-center justify-end pt-sm">
             <button

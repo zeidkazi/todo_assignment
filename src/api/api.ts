23e103628346c97c35de927bd5358174dfc6792b
ZEID KAZI <zeidkazi7860@gmail.com>
2025-02-17 12:44:32 +0530

    chore: edit apis

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,4 @@
+import toast from "react-hot-toast";
 import { AddTodoType, EditTodoType, FetchTodoType } from "../types";
 
 export const addTodo = async (data: AddTodoType) => {
@@ -11,8 +12,10 @@ export const addTodo = async (data: AddTodoType) => {
   return resData;
 };
 
-export const fetchTodo = async (page:number ) => {
-  const response = await fetch(`http://localhost:3000/todos?${page ? `_page=${page}&_per_page=6`: ``}`);
+export const fetchTodo = async (page: number) => {
+  const response = await fetch(
+    `http://localhost:3000/todos?${page ? `_page=${page}&_per_page=6` : ``}`
+  );
   const data = await response.json();
   return data;
 };
@@ -22,15 +25,17 @@ export const deleteTodo = async (id: number) => {
     method: "DELETE",
   });
   const data = await response.json();
+  toast.success("Deleted Todo");
   return data;
 };
 
-export const editTodo = async ({id,todo }: EditTodoType) => {
+export const editTodo = async ({ id, todo }: EditTodoType) => {
   const response = await fetch(`http://localhost:3000/todos/${id}`, {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(todo),
   });
   const resData = await response.json();
+  toast.success("Edited ToDo");
   return resData;
 };
